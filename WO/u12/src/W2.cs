d450a419381fe02deaecea75af68ac6cbc37f096
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-01-23 14:10:24 +0000

    🫦

--- WO/u12/src/W2.cs
+++ WO/u12/src/W2.cs
@@ -5,9 +5,42 @@ namespace u12.src
     {
         public static void Func1()
         {
+            // Part of this work is continued from exc1
             Utils.Label1("Method");
-            Console.WriteLine("Hello World! From .. Press any button to exit");
-            Console.ReadKey();
+
+            var userChoice = Console.ReadLine();
+
+            if (userChoice == "S")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Seleted Option: See TODOs");
+                Console.ResetColor();
+            }
+            else if (userChoice == "A")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Seleted Option: Add TODO");
+                Console.ResetColor();
+            }
+            else if (userChoice == "R")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Seleted Option: Remove TODO");
+                Console.ResetColor();
+            }
+            else if (userChoice == "E")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Seleted Option: Exit");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Choice");
+                Console.ResetColor();
+            }
         }
+
     }
 }
