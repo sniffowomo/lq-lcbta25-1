cd944b712b7a69dd38b61d99566ad5f589fa43a5
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-01-24 14:35:43 +0000

    🫦

--- IMG/buty1/Program.cs
+++ IMG/buty1/Program.cs
@@ -1,12 +1,20 @@
 ﻿// File for curling 
 
+using Pastel;
+using System.Drawing;
+
 namespace buty1
 {
     class MainClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var colors = new[] { Color.Red, Color.AliceBlue, Color.BlanchedAlmond, Color.BlueViolet };
+            foreach (var color in colors)
+            {
+                Console.WriteLine("Hello World!".Pastel(color));
+            }
+
         }
     }
 }
